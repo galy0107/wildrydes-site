ecd79ff7e3c0410a78852acd5330dca783102d3c
galy0107 <106386092+galy0107@users.noreply.github.com>
2022-05-28 12:07:46 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-1_lbxs3kR03
+        userPoolClientId: '', // e.g. 5gsvdai0u049ljqm005djf4p62
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
